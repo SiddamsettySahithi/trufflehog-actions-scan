ee4d4f75c466c4a7f2b2a3a6746ef142a12a9cfd
SiddamsettySahithi <84909708+SiddamsettySahithi@users.noreply.github.com>
2021-08-05 16:36:40 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,18 @@
 
 set -e # Abort script at first error
 
-args=" --no-entropy --format=html --output=secrets.html " # Default trufflehog options
-
-if [ -n "${INPUT_SCANARGUMENTS}" ]; then
-  args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
-fi
+args=" --rules=regexes.json --no-entropy --format=html --output=secrets.html " # Default trufflehog options
 
 # By default the 'WORKDIR' of our Docker image is set to the 'GITHUB_WORKSPACE'
 # which is mounted into our image. This means, as long as a checkout action was
 # done before our action runs, we'll have access to the repository.
-githubRepo="." # Default target repository
-query="$args $githubRepo" # Build args query with repository url
-trufflehog3 $query
+#githubRepo="." # Default target repository
+#query="$args $githubRepo" # Build args query with repository url
+#trufflehog3 $query
+
+if [ -n "${INPUT_BRANCH}" ]; then
+  args="${args} --branch ${INPUT_BRANCH}"
+fi
+
+cp /regexes.json .
+/usr/local/bin/trufflehog3 ${args} .
